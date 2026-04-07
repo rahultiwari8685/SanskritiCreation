ba96b05c309eb84b1e7a70a771811b54037de57b
rahultiwari8685 <rtiwari69572@gmail.com>
2026-04-07 15:52:52 +0530

    cors updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,18 +32,7 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-app.use(
-  cors({
-    origin: [
-      "https://sanskritisutracreations.com",
-      "https://admin.sanskritisutracreations.com",
-    ],
-    credentials: true,
-  }),
-);
-
-// app.options(
-//   "*",
+// app.use(
 //   cors({
 //     origin: [
 //       "https://sanskritisutracreations.com",
@@ -52,6 +41,29 @@ app.use(
 //     credentials: true,
 //   }),
 // );
+
+const allowedOrigins = [
+  "https://sanskritisutracreations.com",
+  "https://admin.sanskritisutracreations.com",
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json({ limit: "1mb" }));
 
