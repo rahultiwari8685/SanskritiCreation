4f67c0005f588d3763c390e26e7e5964c465f357
rahultiwari8685 <rtiwari69572@gmail.com>
2026-04-08 16:14:25 +0530

    shopProduct issue resolved

--- frontend/pages/shop/index.js
+++ frontend/pages/shop/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import ShopSidebar from "./ShopSidebar";
+
+import ShopProduct from "../../components/ShopProduct";
 
 const ShopProduct = ({ products, addToCartProduct }) => {
   const ClickHandler = () => {
