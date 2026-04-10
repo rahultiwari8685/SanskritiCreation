bcd17a77ab637ec06ddaa80bb03d185cf2387de1
rahultiwari8685 <rtiwari69572@gmail.com>
2026-04-10 13:27:31 +0530

    shop page dynamic

--- frontend/components/ShopProduct/index.js
+++ frontend/components/ShopProduct/index.js
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import ShopSidebar from "./ShopSidebar";
 
-const ShopProduct = ({ products, addToCartProduct }) => {
+const ShopProduct = ({ products }) => {
   const ClickHandler = () => {
     window.scrollTo(10, 0);
   };
 
-  const [sortOption, setSortOption] = useState("1");
   const [currentPage, setCurrentPage] = useState(1);
   const resultsPerPage = 12;
 
   const totalResults = products.length;
   const totalPages = Math.ceil(totalResults / resultsPerPage);
-
-  const handleSortChange = (e) => {
-    setSortOption(e.target.value);
-  };
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
@@ -30,31 +24,27 @@ const ShopProduct = ({ products, addToCartProduct }) => {
   );
 
   const [activeTab, setActiveTab] = useState("Tab1");
-  const openTab = (TabName) => {
-    setActiveTab(TabName);
-  };
 
   useEffect(() => {
-    openTab("Tab1");
+    setActiveTab("Tab1");
   }, []);
 
   return (
     <section className="shop-page-section fix section-padding section-bg-2">
       <div className="container">
         <div className="row g-4">
-          <div className="col-xl-3 col-lg-4 order-2 order-md-1">
-            <ShopSidebar />
-          </div>
-          <div className="col-xl-9 col-lg-8 order-1 order-md-2">
+          <div className="col-xl-12 col-lg-12">
+            {/* TOP BAR */}
             <div className="woocommerce-notices-wrapper">
               <p>
                 Showing <span>{resultsPerPage}</span> of {totalResults} Results
               </p>
+
               <div className="form-clt">
                 <div className="icon">
                   <button
                     className={`tab ${activeTab === "Tab1" ? "active" : ""}`}
-                    onClick={() => openTab("Tab1")}
+                    onClick={() => setActiveTab("Tab1")}
                   >
                     <i className="fas fa-list"></i>
                   </button>
@@ -62,7 +52,7 @@ const ShopProduct = ({ products, addToCartProduct }) => {
                 <div className="icon">
                   <button
                     className={`tab ${activeTab === "Tab2" ? "active" : ""}`}
-                    onClick={() => openTab("Tab2")}
+                    onClick={() => setActiveTab("Tab2")}
                   >
                     <i className="fas fa-th-large"></i>
                   </button>
@@ -70,139 +60,93 @@ const ShopProduct = ({ products, addToCartProduct }) => {
               </div>
             </div>
 
+            {/* GRID VIEW */}
             <div className="row">
               {currentProducts.length > 0 &&
-                currentProducts.map((product, pitem) => (
+                currentProducts.map((product) => (
                   <div
                     className="col-lg-4 col-md-6 col-12"
-                    key={pitem}
+                    key={product._id}
                     style={{ display: activeTab === "Tab1" ? "block" : "none" }}
                   >
                     <div className="product-box-items">
                       <div className="product-image">
-                        <img src={product.proImg} alt="img" />
-                        <ul className="product-icon d-grid align-items-center">
-                          <li>
-                            <button
-                              onClick={(ClickHandler) =>
-                                addToCartProduct(product)
-                              }
-                            >
-                              <i className="fa-sharp fa-regular fa-eye"></i>
-                            </button>
-                          </li>
-                          <li>
-                            <Link onClick={ClickHandler} href="#">
-                              <i className="fa-regular fa-star"></i>
-                            </Link>
-                          </li>
-                          <li>
-                            <Link
-                              onClick={ClickHandler}
-                              href={"/shop-details/[slug]"}
-                              as={`/shop-details/${product.slug}`}
-                            >
-                              <i className="fa-regular fa-arrow-up-arrow-down"></i>
-                            </Link>
-                          </li>
-                        </ul>
+                        <img
+                          src={`https://api.sanskritisutracreations.com/uploads/images/${product.thumbnail}`}
+                          alt={product.title}
+                        />
+
                         <div className="shop-btn">
-                          <button
-                            onClick={() => addToCartProduct(product)}
+                          <Link
+                            onClick={ClickHandler}
+                            href={`/shop-details/${product._id}`}
                             className="theme-btn"
                           >
-                            Add To Cart
-                          </button>
+                            Show Details
+                          </Link>
                         </div>
                       </div>
+
                       <div className="product-content">
-                        <div className="star">
-                          <i className="fa-solid fa-star"></i>
-                          <i className="fa-solid fa-star"></i>
-                          <i className="fa-solid fa-star"></i>
-                          <i className="fa-solid fa-star"></i>
-                          <i className="color-2 fa-solid fa-star"></i>
-                        </div>
                         <h6>
                           <Link
                             onClick={ClickHandler}
-                            href={"/shop-details/[slug]"}
-                            as={`/shop-details/${product.slug}`}
+                            href={`/shop-details/${product._id}`}
                           >
                             {product.title}
                           </Link>
                         </h6>
-                        <span>{product.price}</span>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-            </div>
-            <div className="row">
-              {currentProducts.length > 0 &&
-                currentProducts.slice(0, 5).map((product, pitem) => (
-                  <div
-                    className="col-lg-6 col-md-12"
-                    key={pitem}
-                    style={{ display: activeTab === "Tab2" ? "block" : "none" }}
-                  >
-                    <div className="product-box-items">
-                      <div className="product-image">
-                        <img src={product.proImg} alt="img" />
-                        <ul className="product-icon d-grid align-items-center">
-                          <li>
-                            <button onClick={() => addToCartProduct(product)}>
-                              <i className="fa-sharp fa-regular fa-eye"></i>
-                            </button>
-                          </li>
-                          <li>
-                            <Link onClick={ClickHandler} href="#">
-                              <i className="fa-regular fa-star"></i>
-                            </Link>
-                          </li>
-                          <li>
-                            <Link
-                              onClick={ClickHandler}
-                              href={"/shop-details/[slug]"}
-                              as={`/shop-details/${product.slug}`}
-                            >
-                              <i className="fa-regular fa-arrow-up-arrow-down"></i>
-                            </Link>
-                          </li>
-                        </ul>
-                        <div className="shop-btn">
-                          <button
-                            onClick={() => addToCartProduct(product)}
-                            className="theme-btn"
-                          >
-                            Add To Cart
-                          </button>
-                        </div>
-                      </div>
-                      <div className="product-content">
-                        <div className="star">
-                          <i className="fa-solid fa-star"></i>
-                          <i className="fa-solid fa-star"></i>
-                          <i className="fa-solid fa-star"></i>
-                          <i className="fa-solid fa-star"></i>
-                          <i className="color-2 fa-solid fa-star"></i>
-                        </div>
-                        <h6>
-                          <Link
-                            onClick={ClickHandler}
-                            href={"/shop-details/[slug]"}
-                            as={`/shop-details/${product.slug}`}
-                          >
-                            {product.title}
-                          </Link>
-                        </h6>
-                        <span>{product.price}</span>
+                        <span>₹{product.price}</span>
                       </div>
                     </div>
                   </div>
                 ))}
             </div>
 
+            {/* LIST VIEW */}
+            <div className="row">
+              {currentProducts.length > 0 &&
+                currentProducts.map((product) => (
+                  <div
+                    className="col-lg-6 col-md-12"
+                    key={product._id}
+                    style={{ display: activeTab === "Tab2" ? "block" : "none" }}
+                  >
+                    <div className="product-box-items">
+                      <div className="product-image">
+                        <img
+                          src={`https://api.sanskritisutracreations.com/uploads/images/${product.thumbnail}`}
+                          alt={product.title}
+                        />
+
+                        <div className="shop-btn">
+                          <Link
+                            onClick={ClickHandler}
+                            href={`/shop-details/${product._id}`}
+                            className="theme-btn"
+                          >
+                            Show Details
+                          </Link>
+                        </div>
+                      </div>
+
+                      <div className="product-content">
+                        <h6>
+                          <Link
+                            onClick={ClickHandler}
+                            href={`/shop-details/${product._id}`}
+                          >
+                            {product.title}
+                          </Link>
+                        </h6>
+                        <span>₹{product.price}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </div>
+
+            {/* PAGINATION */}
             <div className="page-nav-wrap mt-5 text-center">
               <ul>
                 <li>
@@ -216,16 +160,20 @@ const ShopProduct = ({ products, addToCartProduct }) => {
                     <i className="fa-solid fa-chevrons-left"></i>
                   </button>
                 </li>
+
                 {Array.from({ length: totalPages }, (_, i) => (
                   <li key={i}>
                     <button
-                      className={`page-numbers ${currentPage === i + 1 ? "active" : ""}`}
+                      className={`page-numbers ${
+                        currentPage === i + 1 ? "active" : ""
+                      }`}
                       onClick={() => handlePageChange(i + 1)}
                     >
                       {i + 1}
                     </button>
                   </li>
                 ))}
+
                 <li>
                   <button
                     className="page-numbers"
--- frontend/pages/shop/index.js
+++ frontend/pages/shop/index.js
@@ -4,7 +4,7 @@ import PageTitle from "../../components/pagetitle/PageTitle";
 import { addToCart } from "../../store/actions/action";
 import ShopProduct from "../../components/ShopProduct";
 import api from "../../api";
-import NavbarS2 from "../../components/NavbarS2/NavbarS2";
+import NavbarS3 from "../../components/NavbarS3/NavbarS3";
 import CtaSectionS2 from "../../components/CtaSectionS2/CtaSectionS2";
 import FooterS3 from "../../components/footerS3/FooterS3";
 import CursorMaus from "../../components/CursorMaus/CursorMaus";
@@ -20,7 +20,7 @@ const ShopPage = ({ addToCart }) => {
 
   return (
     <Fragment>
-      <NavbarS2 hclass={"header-section-2 style-two"} />
+      <NavbarS3 hclass={"header-section-2 style-two"} />
       <PageTitle pageTitle={"Digital printing Service"} pagesub={"Shop"} />
       <ShopProduct addToCartProduct={addToCartProduct} products={products} />
       <CtaSectionS2 />
